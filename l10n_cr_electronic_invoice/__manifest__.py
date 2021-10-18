906c9843bcccb180fd4ad93f3ca3267915284983
jhonny16 <tinny.1609@gmail.com>
2021-10-18 15:16:14 -0500

    v 14.0.6.10

--- l10n_cr_electronic_invoice/__manifest__.py
+++ l10n_cr_electronic_invoice/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "FECR",
-    "version": "14.0.6.9",
+    "version": "14.0.6.10",
     "category": "Accounting",
     "summary": "Factura electrónica para Costa Rica",
     "author": "XALACHI",
