158a1886082aea66b7a24c589490f458ef87df27
jhonny16 <tinny.1609@gmail.com>
2021-11-05 15:47:49 -0500

    v: 14.0.7.4

--- l10n_cr_electronic_invoice/models/__init__.py
+++ l10n_cr_electronic_invoice/models/__init__.py
@@ -11,7 +11,7 @@ from . import identification_type
 from . import product_template
 from . import res_company
 from . import res_config_settings
-from . import account_invoice_import_config
+from . import account_move_import_config
 from . import res_partner_tax
 from . import res_partner
 from . import resolution
